588191ecfb3be9deb4d0f764ef98f5b56baa01b3
ajl5uu <123408221+ajl5uu@users.noreply.github.com>
2023-11-30 16:03:48 -0500

    UI pages

---
+++ restaurants.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Restaurants</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Restaurants List</h2>
+  <p>The restaurants in the database are listed below:</p>            
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Location</th>
+        <th>Category</th>
+	<th>Owner</th>
+	<th>Reviews</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Afghan Kabob</td>
+        <td>400 Emmet St</td>
+	<td>category here</td>
+        <td>Owner</td>
+	<td>link to reviews (populate table)</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
