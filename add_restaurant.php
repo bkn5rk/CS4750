3a8eb5f4d9728adb34fb976d521ef8d58d562f07
Bao Nguyen <bkn5rk@virginia.edu>
2023-12-03 20:21:01 -0500

    Search function working in add restaurant

--- add_restaurant.php
+++ add_restaurant.php
@@ -1,34 +1,49 @@
+<?php
+session_start();
+require("connect-db.php");
+// include("connect-db.php");
+
+require("restaurant-db.php");
+$list_of_restaurants = getAllRestaurants();
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Login</title>
+  <title>Restaurants</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-
 <body>
 
 <?php 
-$currentPage='add_phone'; 
-session_start();
+$currentPage = 'add_restaurants'; 
 $loggedIn = isset($_SESSION['username']) && $_SESSION['username'] != '';
 include('navbar.php'); 
-
-  require("connect-db.php");
-// include("connect-db.php");
-
-require("restaurant-db.php");
-#var_dump($_SESSION);
-$list_of_restaurants = getAllRestaurants();
-
-
 ?>
- 
- <div class="container">
+
+<div class="container">
   <h2>Restaurants List</h2>
+  <p>Type a name in the input field to search for a specific restaurant:</p>
+  <input class="form-control" id="myInput" type="text" placeholder="Search..">
+  <br>
+  <div id="myDIV">
   <?php echo $_SESSION['username']; ?>
   <p>The restaurants in the database are listed below:</p>            
   <table class="table table-hover">
@@ -42,9 +57,10 @@ $list_of_restaurants = getAllRestaurants();
     </thead>
     <tbody>
     <form action="/CS4750/add_restaurantp2.php" method = "POST"> 
+
     <?php foreach ($list_of_restaurants as $restaurant): ?>
-  <tr>
-     <td><?php echo $restaurant['rest_name']; ?> </td>
+    <tr id="<?php echo $restaurant['rest_name']?>">
+     <td><a href="/CS4750/a-restaurant.php?restid=<?php echo $restaurant['restaurant_id']; ?>"><?php echo $restaurant['rest_name']; ?> </a></td>
      <td><?php echo $restaurant['location']; ?></td>
      <td><?php 
             $rest_categories=getCategories($restaurant['restaurant_id']); 
@@ -56,15 +72,28 @@ $list_of_restaurants = getAllRestaurants();
           ?></td>
     <td> <input type="checkbox" id= 'Own' name= 'Own[]' value=<?php echo $restaurant['restaurant_id'] ?>>
 
-  </tr>
+    </tr>
 <?php endforeach; ?>
-
-
     </tbody>
   </table>
   <input type="submit" value="Add Restaurant that you Own" class="btn btn-info" />            
     </form>
 </div>
+</div>
+
+<script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myDIV *").filter(function() {
+      if ($(this).is("tr") && $(this).is("[id]")) {
+        $(this).toggle($(this).attr("id").toLowerCase().indexOf(value) > -1);
+      }
+    });
+  });
+});
+</script>
 
 </body>
 </html>
+
